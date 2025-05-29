d1cc83e00f76353770e33ad76f35ae1804f6bb1e
Vadim <_s_e_t__@ukr.net>
2025-05-29 16:36:39 +0300

    add some code

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,12 +1,50 @@
+import { verifyPassword } from "@/lib/auth";
+import { connectToDatabase } from "@/lib/db";
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
 export default NextAuth({
-    providers: [
-        CredentialsProvider({
-           async authorize(credentials,req) {
-                
-            }
-        })
-    ]
-})
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const client = await connectToDatabase();
+        if (!client) {
+          throw new Error("Failed to connect to the database");
+        }
+        const usersCollection = client.db().collection("users");
+
+        const user = await usersCollection.findOne({
+          email: credentials?.email,
+        });
+
+        client.close();
+
+        if (!user) {
+          throw new Error("No user found!");
+        }
+
+        const isValid = await verifyPassword(
+          credentials!.password,
+          user.password
+        );
+
+        if (!isValid) {
+          throw new Error("Could not log you in!");
+        }
+
+        return {
+          id: user._id.toString(),
+          email: user.email,
+          name: user.name,
+        };
+      },
+    }),
+  ],
+});
